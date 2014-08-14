a9d5e18979d5cce0b1cb8a962d42e60d978e7fe4
Zsolt Lengyel <zsolt.lengyel.it@gmail.com>
2014-08-14 21:55:29 +0200

    Wiidget tests

--- wiidget/src/main/java/org/landa/wiidget/WiidgetView.java
+++ wiidget/src/main/java/org/landa/wiidget/WiidgetView.java
@@ -4,24 +4,27 @@ import org.landa.wiidget.context.WiidgetContext;
 import org.landa.wiidget.engine.WiidgetFactory;
 import org.landa.wiidget.io.BufferedPrintStream;
 
-
-
+/**
+ * The view is the main entry point of document.
+ * @author Zsolt Lengyel (zsolt.lengyel.it@gmail.com)
+ *
+ */
 public abstract class WiidgetView extends Wiidget {
 
 	private BufferedPrintStream printStream;
 
 	private WiidgetFactory widgetFactory;
-	
-	public WiidgetView() {
+
+	protected WiidgetView() {
 		this.printStream = new BufferedPrintStream();
 	}
 
 	public WiidgetView(final WiidgetFactory wiidgetFactory) {
 		this();
-		this.widgetFactory = wiidgetFactory;	
+		this.widgetFactory = wiidgetFactory;
 	}
-	
-	public void clearPrintStream(){
+
+	public void clearPrintStream() {
 		this.printStream = new BufferedPrintStream();
 	}
 
---
+++ wiidget/src/test/java/org/landa/wiidget/WiidgetViewTest.java
@@ -0,0 +1,11 @@
+package org.landa.wiidget;
+
+
+/**
+ *
+ * @author Zsolt Lengyel (zsolt.lengyel.it@gmail.com)
+ *
+ */
+public class WiidgetViewTest {
+
+}
